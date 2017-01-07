7b75bd8bbbc8d27304f63fdcd1750d97c2bff8c7
QuocVoMinh <quoc.vominh1995@gmail.com>
2017-01-07 12:54:38 +0700

    Change

--- OnlineCourseCommunity/Global.asax.cs
+++ OnlineCourseCommunity/Global.asax.cs
@@ -19,8 +19,8 @@ namespace OnlineCourseCommunity
             if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
             {
                 HttpContext.Current.Response.AddHeader("Cache-Control", "no-cache");
-                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST");
-                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "*");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "*");
                 HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
                 HttpContext.Current.Response.End();
             }
