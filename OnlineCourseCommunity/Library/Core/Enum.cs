90455b5a87f8816f68280406090647ca2158fa6c
QuocVoMinh <quoc.vominh1995@gmail.com>
2017-01-08 09:10:09 +0700

    Update Category Enum

--- OnlineCourseCommunity/Library/Core/Enum.cs
+++ OnlineCourseCommunity/Library/Core/Enum.cs
@@ -5,7 +5,11 @@ namespace OnlineCourseCommunity.Library.Core
     public enum Category
     {
         [Description("Development")]
-        Development = 0
+        Development = 0,
+        [Description("IT & Software")]
+        ItAndSoftWare = 1,
+        [Description("Design")]
+        Design = 2,
     }
     public enum SubCategory
     {
